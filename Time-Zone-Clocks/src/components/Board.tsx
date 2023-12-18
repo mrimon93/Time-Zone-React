95bec42bc6377749af4b771b2b9ba368edbac35b
mrimon93 <muzammil_kamaly93@live.se>
2023-12-18 19:24:37 +0100

    updating the file

--- Time-Zone-Clocks/src/components/Board.tsx
+++ Time-Zone-Clocks/src/components/Board.tsx
@@ -1,7 +1,19 @@
+import React from 'react'
+import Clock from './Clock'
 
 interface BoardProps {
     clockConfigs: {
         timeZone: string;
         location: string;
     }[];
+}
+
+const Board: React.FC<BoardProps> = ({ clockConfigs }) => {
+    return(
+        <div>
+            {clockConfigs.map((config, index) => {
+              <Clock key = {index} timeZone = {config.timeZone} location {config.location} />
+            })}
+        </div>
+    )
 }
--- Time-Zone-Clocks/src/components/Clock.tsx
+++ Time-Zone-Clocks/src/components/Clock.tsx
@@ -1,5 +1,24 @@
+import { addSeconds } from "date-fns";
+import { utcToZonedTime } from "date-fns-tz";
+import { useEffect, useState } from "react";
 
 interface ClockProps{
     timeZone: string;
     location: string;
 }
+
+const Clock: React.FC<ClockProps> = ({ timeZone, location }) => {
+    const[currentTime, setCurrentTime] = useState(new Date());
+
+
+    useEffect(() => {
+        const intervalId = setInterval(() => {
+            const now = new Date();
+            const zonedTime = utcToZonedTime(now, timeZone);
+            setCurrentTime(addSeconds(zonedTime, 1));
+        }, 1000)
+    })
+}
+
+
+export default Clock;
